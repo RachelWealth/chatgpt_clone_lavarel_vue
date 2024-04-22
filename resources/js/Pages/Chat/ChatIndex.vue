bd81e8ba4f390b3759a1a46db55120afb4f2032c
RachelWealth <74123687+RachelWealth@users.noreply.github.com>
2024-04-22 23:40:08 +0200

    Done

--- resources/js/Pages/Chat/ChatIndex.vue
+++ resources/js/Pages/Chat/ChatIndex.vue
@@ -3,8 +3,8 @@ import ChatLayout from "@/Layouts/ChatLayout.vue";
 import { Link, useForm } from "@inertiajs/vue3";
 import { defineProps, ref, onMounted  } from "vue";
 import ChatContent from "@/Components/ChatContent.vue";
-
-
+import Skelecon from "@/Components/Skelecon.vue"
+ 
 const promtInput = ref(null);
 const chatContainer = ref(null);
 const showDeleteButton = ref(false);
@@ -50,15 +50,108 @@ onMounted(() => {
     <ChatLayout>
         <template #aside>
             <ul class="p-2 w-full ">
+                <li
+                    v-if="chat"
+                    class="px-4 py-2 my-2 flex justify-between font-semibold text-green-400 bg-slate-900 hover:bg-slate-700 rounded-lg duration-200"
+                >
+                    <Link href="/chat" class="w-full">New Chat</Link>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M12 4.5v15m7.5-7.5h-15"
+                        />
+                    </svg>
+                </li>
                 <template v-for="message in messages" :key="message.id">
                     <li
-                        class="px-4 w-full py-2 my-2 flex justify-between font-semibold text-white bg-slate-900 hover:bg-slate-700 rounded-lg duration-200">
+                        :class="[
+                            message.id === chat?.id
+                                ? 'bg-slate-700'
+                                : 'bg-slate-900',
+                            'px-4 py-2 my-2 flex justify-between font-semibold text-slate-400 hover:bg-slate-700 rounded-lg duration-200',
+                        ]"
+                    >
                         <Link :href="`/chat/${message.id}`">
-                        {{ message.context.length > 0 ? message.context[0].content : 'New chat' }}
+                           {{ message.context.length > 0 ? message.context[0].content : 'New chat' }}
                         </Link>
+                        <div v-if="message.id === chat?.id">
+                            <button
+                                v-if="!showDeleteButton"
+                                @click="showDeleteButton = !showDeleteButton"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    stroke="currentColor"
+                                    class="w-6 h-6 text-red-300"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                                    />
+                                </svg>
+                            </button>
+                            <span
+                                v-if="showDeleteButton"
+                                class="flex justify-between"
+                            >
+                                <Link
+                                    :href="route('chat.destroy', chat?.id)"
+                                    method="DELETE"
+                                    as="button"
+                                    class="text-red-300 hover:text-red-500"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="1.5"
+                                        stroke="currentColor"
+                                        class="w-6 h-6"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            d="M4.5 12.75l6 6 9-13.5"
+                                        />
+                                    </svg>
+                                </Link>
 
+                                <button
+                                    @click="showDeleteButton = false"
+                                    class="text-slate-300 hover:text-slate-500"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="1.5"
+                                        stroke="currentColor"
+                                        class="w-6 h-6"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            d="M6 18L18 6M6 6l12 12"
+                                        />
+                                    </svg>
+                                </button>
+                            </span>
+                        </div>
                     </li>
                 </template>
+             
             </ul>
         </template>
         <div id="content" class="w-full flex text-white">
@@ -68,6 +161,7 @@ onMounted(() => {
                         <template v-for="(content, index) in chat?.context" :key="index">
                             <ChatContent :content="content"> </ChatContent>
                         </template>
+                        <Skelecon v-show="form.processing" />
                     </div>
                 </div>
             </template>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\ChatGptDestroyController;
 use App\Http\Controllers\ChatgptIndexController;
 use App\Http\Controllers\ChatGptStoreController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Models\Chat;
 
 // Route::get('/', function () {
 //     return Inertia::render('Welcome', [
@@ -31,13 +32,15 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/chat/{id?}', function ($id = null) {
         if ($id === null) {
-            return redirect()->route('chat.show', ['id' => 0]);
+            $highestId = Chat::max('id');
+            return redirect()->route('chat.show', ['id' => $highestId]);
         }
-    
         return (new ChatgptIndexController())-> __invoke($id);
     })->name('chat.show');
+    
     Route::post('/chat/{id?}',ChatGptStoreController::class)->name('chat.store');
-
+    Route::delete('/chat/{chat?}', ChatGptDestroyController::class)->name('chat.destroy');
+    
     });
 
 require __DIR__.'/auth.php';
